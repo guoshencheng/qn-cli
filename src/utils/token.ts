234a53eff39d2d2b3333637f11bb15ae830f9db7
guoshencheng <648772021@qq.com>
2018-08-11 13:39:30 +0800

    cache a token in some command

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,18 +2,28 @@ import { readFileSync, writeFileSync } from 'fs'
 import { tokenfile } from './constants';
 import { askTokens } from './ask';
 
+let cachedToken: Token;
+
 export interface Token {
   ak: string;
   sk: string;
 }
 
 export const checkTokens = async (): Promise<Token> => {
-  let tokens;
-  try {
-    tokens = JSON.parse(readFileSync(tokenfile, 'utf8')) as Token;
-  } catch(e) {
-    tokens = await askTokens();
-    writeFileSync(tokenfile, JSON.stringify(tokens), 'utf8');
+  if (cachedToken) {
+    return cachedToken;
   }
-  return tokens;
+  try {
+    cachedToken = JSON.parse(readFileSync(tokenfile, 'utf8')) as Token;
+    return cachedToken;
+  } catch(e) {
+    return resetToken();
+  }
+}
+
+export const resetToken = async (): Promise<Token> => {
+  const token = await askTokens();
+  writeFileSync(tokenfile, JSON.stringify(token), 'utf8');
+  cachedToken = token;
+  return token;
 }
